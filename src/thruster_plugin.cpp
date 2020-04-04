22e9047f6ce3a4338f773216fa71373dabd9703f
lafith <mmlafith3@gmail.com>
2020-04-04 23:28:55 +0530

    pwm to thrust calculation modified

--- src/thruster_plugin.cpp
+++ src/thruster_plugin.cpp
@@ -24,8 +24,13 @@ namespace gazebo{
   private: std::string path="auv_demo::thruster_";
   private: std::string k;
   private: double initial,adjusted;
-  //private: event::ConnectionPtr updateEvent;
+  private: event::ConnectionPtr updateEvent;
   private: double thrusterspeeds_[6]={1500.0,1500.0,1500.0,1500.0,1500.0,1500.0};
+  private: double a=-51.348085781063;
+  private: double b=0.10072267395657193;
+  private: double c=-0.00006784574094879734;
+  private: double d=1.5694002475642106e-8;
+
   public: virtual void Load(physics::ModelPtr _model,sdf::ElementPtr _sdf)
   {
     if(!ros::isInitialized()){
@@ -46,22 +51,22 @@ namespace gazebo{
       "/thruster_speeds",1,
       boost::bind(&ThrusterPlugin::OnRosMsg,this,_1),
       ros::VoidPtr(), &this->tpSubQueue);
-  //    this->updateEvent = event::Events::ConnectWorldUpdateBegin(
-//      std::bind(&ThrusterPlugin::OnFrame, this));
       this->tpSub = this->tpNode->subscribe(so);
 
       // Spin up the queue helper thread.
       this->tpSubQueueThread =
         std::thread(std::bind(&ThrusterPlugin::QueueThread, this));
+        this->updateEvent = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&ThrusterPlugin::OnFrame, this));
   }
   public: void OnRosMsg(const thruster_controller::ThrusterSpeeds::ConstPtr &_msg)
   {
     for(int i=0;i<6;i++){
 //      std::cout<<"BEFORE:"<<i<<" = "<<_msg->data[i]<<std::endl;
-      thrusterspeeds_[i]=AdjustForce((double)_msg->data[i]);
+      thrusterspeeds_[i]=(double)_msg->data[i];
 //      std::cout<<"AFTER:"<<i<<" = "<<thrusterspeeds_[i]<<std::endl;
     }
-    this->thrusters[0]->AddRelativeForce(ignition::math::Vector3d(0, 0, -thrusterspeeds_[0]));
+/*    this->thrusters[0]->AddRelativeForce(ignition::math::Vector3d(0, 0, -thrusterspeeds_[0]));
     this->thrusters[1]->AddRelativeForce(ignition::math::Vector3d(0, 0, -thrusterspeeds_[1]));
     this->thrusters[2]->AddRelativeForce(ignition::math::Vector3d(0, 0, -thrusterspeeds_[2]));
     this->thrusters[3]->AddRelativeForce(ignition::math::Vector3d(thrusterspeeds_[3], 0, 0));
@@ -69,7 +74,7 @@ namespace gazebo{
     this->thrusters[4]->AddRelativeForce(ignition::math::Vector3d(thrusterspeeds_[4], 0, 0));
     this->thrusters[7]->AddRelativeForce(ignition::math::Vector3d(thrusterspeeds_[4], 0, 0));
     this->thrusters[5]->AddRelativeForce(ignition::math::Vector3d(0, -thrusterspeeds_[5], 0));
-
+*/
   }
   /// \brief ROS helper function that processes messages
   private: void QueueThread()
@@ -83,7 +88,7 @@ namespace gazebo{
 
   private: double AdjustForce(double initial){
     double adjusted=0.0;
-    if(initial>=1470.0 && initial <= 1530.0){
+/*    if(initial>=1470.0 && initial <= 1530.0){
         adjusted=0.0;
     }
     else if(initial > 1530.0){
@@ -95,21 +100,29 @@ namespace gazebo{
       adjusted=(initial/370.0)*1.85;
     }
     adjusted=adjusted*9.8*1000;
+    */
+    if(initial>=1470.0 && initial <= 1530.0){
+            adjusted=0.0;
+        }
+else{    adjusted=a+(b*initial)+(c*pow(initial,2.0))+(d*pow(initial,3.0)) ;
+    adjusted = adjusted * 4.44822;}
     return adjusted;
   }
 
-/*public: void OnFrame(){
-//  std::cout<<thrusterspeeds_[0]<<std::endl;
-    this->thrusters[0]->AddRelativeForce(ignition::math::Vector3d(0, 0, -thrusterspeeds_[0]));
-    this->thrusters[1]->AddRelativeForce(ignition::math::Vector3d(0, 0, -thrusterspeeds_[1]));
-    this->thrusters[2]->AddRelativeForce(ignition::math::Vector3d(0, 0, thrusterspeeds_[2]));
-    this->thrusters[3]->AddRelativeForce(ignition::math::Vector3d(-thrusterspeeds_[3], 0, 0));
-    this->thrusters[6]->AddRelativeForce(ignition::math::Vector3d(-thrusterspeeds_[3], 0, 0));
-    this->thrusters[4]->AddRelativeForce(ignition::math::Vector3d(-thrusterspeeds_[4], 0, 0));
-    this->thrusters[7]->AddRelativeForce(ignition::math::Vector3d(-thrusterspeeds_[4], 0, 0));
-    this->thrusters[5]->AddRelativeForce(ignition::math::Vector3d(0, -thrusterspeeds_[5], 0));
+public: void OnFrame(){
 
-}*/
+  this->thrusters[0]->AddRelativeForce(ignition::math::Vector3d(0, 0, -AdjustForce(thrusterspeeds_[0])));
+    this->thrusters[1]->AddRelativeForce(ignition::math::Vector3d(0, 0, -AdjustForce(thrusterspeeds_[1])));
+    this->thrusters[2]->AddRelativeForce(ignition::math::Vector3d(0, 0, -AdjustForce(thrusterspeeds_[2])));
+    this->thrusters[3]->AddRelativeForce(ignition::math::Vector3d(AdjustForce(thrusterspeeds_[3]), 0, 0));
+    this->thrusters[6]->AddRelativeForce(ignition::math::Vector3d(AdjustForce(thrusterspeeds_[3]), 0, 0));
+    this->thrusters[4]->AddRelativeForce(ignition::math::Vector3d(AdjustForce(thrusterspeeds_[4]), 0, 0));
+    this->thrusters[7]->AddRelativeForce(ignition::math::Vector3d(AdjustForce(thrusterspeeds_[4]), 0, 0));
+    this->thrusters[5]->AddRelativeForce(ignition::math::Vector3d(0, -AdjustForce(thrusterspeeds_[5]), 0));
+//    for(int i=0;i<2;i++){
+//    std::cout<<"Adjusted Thrust: "<<i<<" = "<<AdjustForce(thrusterspeeds_[i])<<std::endl;
+//}
+}
 
 
   };
